efb55ccfc7fe448b6a10c30fe35cf70913982d01
Trong-Thang Pham <phamtrongthang123@gmail.com>
2022-10-27 08:48:03 -0500

    vectorize blending W

--- tests/test_warp.py
+++ tests/test_warp.py
@@ -14,76 +14,60 @@ import time
 from tmp_utils import SE3, decompose_se3, cal_dist, uniform_sample, average_edge_dist_in_face
 
 
-def blending(xc, nodes, kdtree):
-    assert xc.shape == (1,3)
-    xc = xc.view(3)
-    dists, idx = kdtree.query(xc, k=4)
-    numerator = torch.zeros(8)
-    for i in idx: 
-        wixc = torch.exp(torch.linalg.norm(nodes[i][1] - xc) / (2*nodes[i][-1]**2))
-        qkc = nodes[i][2]*wixc 
-        numerator += qkc 
-    dem = torch.linalg.norm(numerator)
-    return numerator/dem 
+def blending(Xc, dqs, dgw, dgv):
+    repxc = Xc.view(Xc.shape[0], 1, Xc.shape[1]).repeat_interleave(dgv.shape[1],1)
+    assert repxc.shape == dgv.shape 
+    wixc = torch.exp(torch.linalg.norm(dgv - repxc, dim=2) / (2*torch.pow(dgw,2)))
+    assert wixc.shape == dgw.shape
+    qkc = torch.einsum('bi,bik->bk',wixc,dqs)
+    assert qkc.shape == (dqs.shape[0],8)
+    dem = torch.linalg.norm(qkc,dim=1).view(dqs.shape[0], 1)
+    return qkc/dem 
 
-def get_W(xc, Tlw, nodes, kdtree):
-    dqb = blending(xc, nodes, kdtree).view(1,8)
-    T = Tlw @ SE3(dqb)
+def get_W(Xc, Tlw, dqs, dgw, dgv):
+    dqb = blending(Xc, dqs, dgw, dgv).view(Xc.shape[0],8)
+    T = torch.einsum('ij,bjk -> bik',Tlw, SE3(dqb))
     return T 
 
 def test_get_W():
-    t1 = time.time()
-    subsample_rate = 10
-    human = trimesh.load('reconstruct/seq006/mesh.ply')
-    verts, faces = human.vertices, human.faces 
-    verts = torch.tensor(verts.copy())
-    faces = torch.tensor(faces.copy())
-    average_distances = []
-    for f in faces:
-        average_distances.append(average_edge_dist_in_face(f, verts))
-
-    t2 = time.time()
-    print("load mesh: ", t2-t1 )
-    _radius = subsample_rate * torch.mean(torch.tensor(average_distances))
-    # print(verts.shape, faces.shape, _radius)
-    _vertices = verts 
-    _faces = faces
-    t3 = time.time()
-    print("sub * average ", t3 - t2)
-    nodes_v, nodes_idx = uniform_sample(_vertices, _radius)
-    nodes_v_t = torch.tensor(nodes_v)
-    assert nodes_v_t.shape[1] == 3
-    t4 = time.time()
-    print("uniform sample ", t4-t3)
-    # define list of node warp
+    _radius = torch.tensor(0.1)
+    nodes_v_t = torch.randn(52,3)
     _nodes = []  
-    for j in range(len(nodes_v)):
-        _nodes.append([nodes_idx[j],
-                            nodes_v_t[j],
+    for j in range(len(nodes_v_t)):
+        _nodes.append([nodes_v_t[j],
                             torch.tensor([1, 0.00, 0.00, 0.00, 0.00, 0.01, 0.01, 0.00]).float(), # real Tic = T_k-1
                             torch.tensor([1, 0.00, 0.00, 0.00, 0.00, 0.01, 0.01, 0.00]).float(), # tmp T_k for optim
                             2 * _radius])
-    t5 = time.time()
-    print("append nodes ", t5-t4)
-    # construct kd tree
-    _kdtree = KDTree(nodes_v)
-    t6 = time.time() 
-    print("kd tree ", t6-t5)
-    # cache all neighbor node for all vertices in canonical space
-    _neighbor_look_up = []
-    for vert in verts:
-        dists, idx = _kdtree.query(vert, k=4)
-        _neighbor_look_up.append(idx) 
-    t7 = time.time()
-    print("Add look up ", t7 - t6)
-    xc = torch.tensor([[1,1,1]]).float()
+    _kdtree = KDTree(nodes_v_t)
+    bs = 5 
+    knn = 4
+    Xc = torch.rand(bs,3).float()
     Tlw = torch.eye(4)
-    # def energy(xc, Tlw, _nodes, _kdtree) 
-    # khong duoc, phai viet get_w theo kieu truyen thang neighbour luon
+    dqs = []
+    dgw = [] 
+    dgv = []
+    for i in range(Xc.shape[0]):
+        dists, idx = _kdtree.query(Xc[i], k=knn)
+        dq = [] 
+        dw = [] 
+        dv = []
+        for id in idx:
+            dv.append(_nodes[i][0])
+            dq.append(_nodes[i][1])
+            dw.append(_nodes[i][3])
+        dqs.append(torch.stack(dq))
+        dgw.append(torch.stack(dw))
+        dgv.append(torch.stack(dv))
+    dqs = torch.stack(dqs)
+    dgw = torch.stack(dgw)
+    dgv = torch.stack(dgv)
+    assert dqs.shape == (bs,knn,8)
+    assert dgw.shape == (bs,knn)
+    assert dgv.shape == (bs,knn,3)
     # theo toan thi moi W là 1 xc + bo neighbors, nen neu batch xc thi + batch bo neighbor. Vectorize duoc. 
     # xc [bs, 3], neighbor [bs, 8], [bs,3] and [bs, 1]. Chi argnum = bs,8 de lay jac. sau khi tinh xong thi concate ket qua vo jac cuoi cung hoac GN luon neu muon 
-    T = get_W(xc, Tlw, _nodes, _kdtree)
+    T = get_W(Xc, Tlw, dqs, dgw, dgv)
     R, t= decompose_se3(T)
-    xt =  (R@ xc.T.reshape(3,1) + t).T.view(xc.shape)
+    Xt =  (torch.einsum('bij,bj->bi', R, Xc) + t.squeeze(-1))
     
-    assert xt.shape == (1,3)
+    assert Xt.shape == (bs,3)
